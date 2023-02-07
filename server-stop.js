e450ba34a842c3e0ed59a2f3810bb4ff45711133
monkey90 <monkey901109@gmail.com>
2023-02-08 00:40:57 +0900

    test

--- server-stop.js
+++ server-stop.js
@@ -1,5 +1,6 @@
 const io = require('socket.io');
-const socketClient = io.connect('https://node-test-six.vercel.app:3000'); // Specify port if your express server is not using default port 80
+//const socketClient = io.connect('http://localhost:3000'); // Specify port if your express server is not using default port 80
+const socketClient = io.connect('https://node-test-pi.vercel.app/'); // Specify port if your express server is not using default port 80
 
 socketClient.on('connect', () => {
   socketClient.emit('npmStop');
